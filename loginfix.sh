69df39c350aa89b2947d9344a912b3c20db79312
Andrew Soltes <soltesandrew@gmail.com>
2023-03-10 16:11:09 +0800

    change fix

--- loginfix.sh
+++ loginfix.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-for user in ansible root; do
-  for os in ubuntu centos; do
-    for instance in 1 2 3; do
-      ssh-copy-id -o StrictHostKeyChecking=no ${user}@${os}${instance}
-    done
-  done
+# Replace these values with your own
+USERNAME="root"
+SSH_KEY_PATH="~/.ssh/id_rsa.pub"
+SERVER_LIST=("ubuntu1" "ubuntu2" "ubuntu3" "centos1" "centos2" "centos3")
+
+# Loop through each server and add the SSH key
+for SERVER in "${SERVER_LIST[@]}"
+do
+  echo "Adding SSH key to $SERVER..."
+  ssh-copy-id -i $SSH_KEY_PATH $USERNAME@$SERVER
 done
+
+echo "Done!"
+
