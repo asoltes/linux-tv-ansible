a7cdd75623df68146093d05ad03c437f3572859d
Andrew Soltes <soltesandrew@gmail.com>
2023-03-10 16:13:39 +0800

    change path

--- loginfix.sh
+++ loginfix.sh
@@ -2,7 +2,7 @@
 
 # Replace these values with your own
 USERNAME="root"
-SSH_KEY_PATH="~/.ssh/id_rsa.pub"
+SSH_KEY_PATH="/root/.ssh/id_rsa.pub"
 SERVER_LIST=("ubuntu1" "ubuntu2" "ubuntu3" "centos1" "centos2" "centos3")
 
 # Loop through each server and add the SSH key
