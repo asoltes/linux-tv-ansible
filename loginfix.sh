6659b9587bc4015407d22deab422537192e6fa08
Andrew Soltes <soltesandrew@gmail.com>
2023-03-10 11:02:59 +0800

    add test fix

--- loginfix.sh
+++ loginfix.sh
@@ -3,7 +3,7 @@
 for user in ansible root; do
   for os in ubuntu centos; do
     for instance in 1 2 3; do
-      ssh -f password.txt ssh-copy-id -o StrictHostKeyChecking=no ${user}@${os}${instance}
+      ssh-copy-id -o StrictHostKeyChecking=no ${user}@${os}${instance}
     done
   done
 done
